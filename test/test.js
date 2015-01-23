f8818174622748c009c43c88ab7b76bd2e8490bd
Rakesh Pai <rakeshpai@gmail.com>
2015-01-23 17:43:56 +0530

    Added a test for stack with the 'new' keyword

--- test/test.js
+++ test/test.js
@@ -29,7 +29,7 @@ describe("stack-parser", function() {
     	]);
 	});
 
-	it("should parse stacks from Firefox 30+ (with column numbers", function() {
+	it("should parse stacks from Firefox 30+ (with column numbers)", function() {
 		// Stack example taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/Stack#Example
 		var stack = "trace@file:///C:/example.html:9:17\nb@file:///C:/example.html:16:13\na@file:///C:/example.html:19:13\n@file:///C:/example.html:21:9"
 
@@ -98,6 +98,20 @@ describe("stack-parser", function() {
 		parsed.should.have.lengthOf(1);
 		parsed.should.eql([
 			{url: 'http://localhost:3000/stackgen.js', func: 'my function', line:  10, column: 21 }
+		]);
+	});
+
+	it("should parse stacks with the new keyword", function() {
+		// https://github.com/occ/TraceKit/issues/66
+		var stack = "Error: BEEP2\n    new <anonymous>@https://webclient.mini.net/tmp/scripts/index.js:643:11\n    d@https://webclient.mini.net/tmp/scripts/libraries.js:34:265\n    Object.instantiate@https://webclient.mini.net/tmp/scripts/libraries.js:34:394\n    $get@https://webclient.mini.net/tmp/scripts/libraries.js:66:112\n    {anonymous}()@https://webclient.mini.net/tmp/scripts/libraries.js:53:14";
+
+		var parsed = parse(stack);
+		parsed.should.eql([
+			{url: 'https://webclient.mini.net/tmp/scripts/index.js', func: 'new <anonymous>', args: '', line: 643, column: 11},
+			{url: 'https://webclient.mini.net/tmp/scripts/libraries.js', func: 'd', args: '', line: 34, column: 265},
+			{url: 'https://webclient.mini.net/tmp/scripts/libraries.js', func: 'Object.instantiate', args: '', line: 34, column: 394},
+			{url: 'https://webclient.mini.net/tmp/scripts/libraries.js', func: '$get', args: '', line: 66, column: 112},
+			{url: 'https://webclient.mini.net/tmp/scripts/libraries.js', func: '{anonymous}()', args: '', line: 53, column: 14}
 		]);
 	});
 });
